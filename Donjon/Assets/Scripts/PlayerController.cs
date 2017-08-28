9218e1dffc9bb7ace5fe0af36527e501a5cccda1
JulienBe <bertozzijulien@gmail.com>
2017-08-28 11:19:42 -0700

    First commit

---
+++ Donjon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 0.6f;
+	private Vector3 dir = Vector3.zero;
+	private CharacterController controller;
+
+	void Start() {
+		controller = GetComponent<CharacterController> ();
+	}
+
+	void Update() {
+		dir.Set (Input.GetAxis ("Horizontal"), 0f, Input.GetAxis ("Vertical"));
+		dir.Normalize ();
+		dir *= speed * Time.deltaTime;
+		dir = transform.TransformDirection (dir);
+		//controller.Move (dir); 
+		//controller.SimpleMove(dir);
+		controller.Move (dir);
+	}
+
+}
