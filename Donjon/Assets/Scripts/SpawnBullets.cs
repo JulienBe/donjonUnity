77d69eec66137ccfc0798a46b9f4795c62b28212
JulienBe <bertozzijulien@gmail.com>
2017-08-28 12:32:42 -0700

    Spawning bullets

---
+++ Donjon/Assets/Scripts/SpawnBullets.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBullets : MonoBehaviour {
+
+	public GameObject bullet;
+	public Transform spawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			var b = (GameObject)Instantiate (bullet, spawn.position, spawn.rotation);
+			bullet.GetComponent<Rigidbody> ().velocity = bullet.transform.forward * 6f;
+		}
+	}
+}
